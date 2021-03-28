46d130520d1171df08941a0baf723b8a1efaceb9
cyndroy <cyndroyrebello1010@gmail.com>
2021-03-29 01:04:13 +0530

    update

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -15,6 +15,7 @@ class HomeScreen extends StatelessWidget {
       child: Scaffold(
         backgroundColor: HexColor("fcf8e8"),
         body: Container(
+          // height: MediaQuery.of(context).size.,
           child: Column(
             // mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -55,6 +56,65 @@ Widget topBar() {
             fontStyle: FontStyle.italic,
             fontFamily: 'Times New Roman'),
       ),
+    ),
+  );
+}
+
+Widget orderTopBar(BuildContext context) {
+
+
+  return Container(
+    height: 100,
+    child: Stack(
+      children: [
+        Container(
+          height: 100,
+          decoration: BoxDecoration(
+              border: Border.all(width: 0.1, color: HexColor("d4e2d4")),
+              boxShadow: [
+                BoxShadow(
+                    color: HexColor("d4e2d4"),
+                    blurRadius: 20.0,
+                    spreadRadius: 0.5,
+                    offset: Offset(0.0, 10.0))
+              ],
+              color: HexColor("ecb390"),
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30.0),
+                  bottomRight: Radius.circular(30.0))),
+          child: Center(
+            child: Text(
+              "Sip & Snax",
+              style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.black26,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  fontFamily: 'Times New Roman'),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: IconButton(
+              icon: Icon(
+                Icons.arrow_back_sharp,
+                color: Colors.black26,
+                size: 40.0,
+              ),
+              onPressed: () => {Navigator.pop(context)}),
+        ),
+        // Align(alignment: Alignment.bottomRight,
+        //     child: Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(formattedDate,style: TextStyle(
+        //           fontSize: 20.0,
+        //           color: Colors.black54,
+        //           fontWeight: FontWeight.bold,
+        //
+        //           fontFamily: 'Times New Roman'),),
+        //     ))
+      ],
     ),
   );
 }
@@ -123,7 +183,7 @@ Widget tabs(List<Product> products, BuildContext context) {
                             return Padding(
                               padding: const EdgeInsets.all(5.0),
                               child: InkWell(
-                                onTap: () => addData(snacks[index]),
+                                onTap: () => {addData(snacks[index])},
                                 child: Container(
                                   child: Card(
                                     elevation: 5.0,
@@ -272,7 +332,7 @@ class _OrdersState extends State<Orders> {
           child: Column(
             // mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              topBar(),
+              orderTopBar(context),
               orderList(formattedDate, sum, track),
             ],
           ),
@@ -287,6 +347,9 @@ Widget orderList(String date, var sum, Map itemTrack) {
       .collection('userOrders')
       .where("date", isEqualTo: date);
 
+
+  final firestoreInstance = FirebaseFirestore.instance;
+
   var itemList = itemTrack.keys.toList();
   var itemCount = itemTrack.values.toList();
 
@@ -299,50 +362,280 @@ Widget orderList(String date, var sum, Map itemTrack) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return CircularProgressIndicator();
         }
-        if(snapshot.data.docs.length == 0)
-          {
-            return Text("No Orders For Today");
-          }
+        if (snapshot.data.docs.length == 0) {
+          return Text("No Orders For Today");
+        }
         return Column(
           children: [
-            SingleChildScrollView(
-              child: Container(
-                color: Colors.blueGrey,
-                height: 600,
-                child: ListView.builder(
-                    itemCount: snapshot.data.docs.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListTile(
-                        title: Text(snapshot.data.docs
-                            .elementAt(index)
-                            .data()['itemName']
-                            .toString()),
-                        subtitle: Text(snapshot.data.docs.length.toString()),
-                      );
-                    }),
+            Padding(
+              padding: const EdgeInsets.only(top:8.0),
+              child: Container(child: Text("Date: "+date,style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.black54,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Times New Roman'),),),
+            ),
+
+            //Orders
+            Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 0.1, color: HexColor("d4e2d4")),
+                      boxShadow: [
+                        BoxShadow(
+                            color: HexColor("d4e2d4"),
+                            blurRadius: 40.0,
+                            spreadRadius: 0.0,
+                            offset: Offset(0.0, 0.0))
+                      ],
+                      color: HexColor("d4e2d4"),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 8.0, left: 8.0),
+                          child: Text(
+                            'Orders :',
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Times New Roman'),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0, right: 8.0),
+                        child: SingleChildScrollView(
+                          child: Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    width: 0.1, color: HexColor("d4e2d4")),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: HexColor("d4e2d4"),
+                                      blurRadius: 40.0,
+                                      spreadRadius: 0.0,
+                                      offset: Offset(0.0, 0.0))
+                                ],
+                                color: HexColor("d4e2d4"),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15.0))),
+                            height: 400,
+                            child: Scrollbar(
+                              child: ListView.builder(
+                                  itemCount: snapshot.data.docs.length,
+                                  itemBuilder:
+                                      (BuildContext context, int index) {
+                                    return Column(
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                              left: 10.0, top: 8.0, right: 8.0),
+                                          child: Container(
+                                            child: Row(
+                                              children: [
+                                                Text(
+                                                  (index + 1).toString() +
+                                                      ". " +
+                                                      snapshot.data.docs
+                                                          .elementAt(index)
+                                                          .data()['itemName']
+                                                          .toString(),
+                                                  style: TextStyle(
+                                                      fontSize: 15.0,
+                                                      fontFamily:
+                                                          'Times New Roman'),
+                                                ),
+                                                Spacer(),
+                                                Align(
+                                                  alignment: Alignment.topRight,
+                                                  child: Text(
+                                                    snapshot.data.docs
+                                                            .elementAt(index)
+                                                            .data()['itemPrice']
+                                                            .toString() +
+                                                        "₹",
+                                                    style: TextStyle(
+                                                        fontSize: 15.0,
+                                                        fontFamily:
+                                                            'Times New Roman'),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                        Divider(),
+                                      ],
+                                    );
+                                  }),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
+
+            //Sale Summary
+            Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 0.1, color: HexColor("d4e2d4")),
+                      boxShadow: [
+                        BoxShadow(
+                            color: HexColor("d4e2d4"),
+                            blurRadius: 40.0,
+                            spreadRadius: 0.0,
+                            offset: Offset(0.0, 0.0))
+                      ],
+                      color: HexColor("d4e2d4"),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 8.0, left: 8.0),
+                          child: Text(
+                            'Sale Summary:',
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Times New Roman'),
+                          ),
+                        ),
+                      ),
+                      SingleChildScrollView(
+                        child: Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  width: 0.1, color: HexColor("d4e2d4")),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: HexColor("d4e2d4"),
+                                    blurRadius: 40.0,
+                                    spreadRadius: 0.0,
+                                    offset: Offset(0.0, 0.0))
+                              ],
+                              color: HexColor("d4e2d4"),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15.0))),
+                          height: 150,
+                          width: MediaQuery.of(context).size.width,
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 8.0, left: 12.0, bottom: 8.0, right: 8.0),
+                            child: Scrollbar(
+                              child: ListView.builder(
+                                  itemCount: itemList.length,
+                                  itemBuilder:
+                                      (BuildContext context, int index) {
+                                    return Column(
+                                      children: [
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(right: 8.0),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                itemList[index].toString() +
+                                                    " :",
+                                                style: TextStyle(
+                                                    fontSize: 15.0,
+                                                    fontFamily:
+                                                        'Times New Roman'),
+                                              ),
+                                              Spacer(),
+                                              Text(
+                                                itemCount[index].toString() +
+                                                    " pcs",
+                                                style: TextStyle(
+                                                    fontSize: 15.0,
+                                                    fontFamily:
+                                                        'Times New Roman'),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        Divider(),
+                                      ],
+                                    );
+                                  }),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                        // border: Border.all(width: 0.1, color: HexColor("d4e2d4")),
+                        // boxShadow: [
+                        //   BoxShadow(
+                        //       // color: HexColor("d4e2d4"),
+                        //       blurRadius: 10.0,
+                        //       spreadRadius: 0.0,
+                        //       offset: Offset(0.0, 0.0))
+                        // ],
+                        // color: HexColor("ecb499"),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: Text(
+                          "Total Sale: " + sum.toString() + "₹",
+                          style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.black45,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Times New Roman'),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                        // border: Border.all(width: 0.1, color: HexColor("d4e2d4")),
+
+                        color: HexColor("ecb390"),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: InkWell(
+                        onTap: ()=>{
+                                      firestoreInstance.collection("Sale").add({"date" : date, "totalSale" : sum,}),
+                                   },
+                        child: Center(
+                        child: Text(
+                          "Save",
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.black45,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Times New Roman'),
+                        ),
+                      ),),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Container(
-              child: Text(sum.toString()),
-            ),
-            SingleChildScrollView(
-              child: Container(
-                height: 100,
-                child: ListView.builder(
-                    itemCount: itemList.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text(itemList[index].toString() + " : "),
-                          Text(itemCount[index].toString()),
-                        ],
-                      );
-                    }),
-              ),
-            )
           ],
         );
       });
 }
-
